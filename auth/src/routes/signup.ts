64c1575eff54d65c7a1b226bbf9201a87617576d
Christofer Assis <chris.f.assis18@gmail.com>
2023-09-26 18:05:46 -0300

    feat: add validation middlewares

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,12 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { signUpValidator } from '../middlewares/validators';
 
 export const signUpRouter = Router();
 
-signUpRouter.post('/signUp', (req, res) => {});
+signUpRouter.post(
+	'/signUp',
+	signUpValidator,
+	(request: Request, response: Response) => {
+		const { email, password } = request.body;
+	},
+);
