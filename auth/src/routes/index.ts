a6b77fb8d95924ba03ddb6a1ddb749f1586bae5e
Christofer Assis <chris.f.assis18@gmail.com>
2023-09-26 16:57:37 -0300

    refactor: auth routers

--- auth/src/routes/index.ts
+++ auth/src/routes/index.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
-import { usersRouter } from './users';
+import { signInRouter } from './signin';
+import { signOutRouter } from './signout';
+import { signUpRouter } from './signup';
+import { currentUserRouter } from './current-user';
 
 export const router = Router();
 
-router.use('/api', usersRouter);
+router.use('/api/users', signInRouter);
+router.use('/api/users', signOutRouter);
+router.use('/api/users', signUpRouter);
+router.use('/api/users', currentUserRouter);
