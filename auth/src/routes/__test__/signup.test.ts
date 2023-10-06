0c1dcc1f248c4a98e7b6a9d631f67efd5aa3f8a5
Christofer Assis <chris.f.assis18@gmail.com>
2023-10-06 00:28:33 -0300

    test: add E2E test for signup route

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,46 @@
+import request from 'supertest';
+import { HttpStatusCode } from '../../@types';
+import { app } from '../../app';
+
+describe('POST /api/users/signup', () => {
+	it('should return 201 on successful signup', async () => {
+		const response = await request(app).post('/api/users/signup').send({
+			email: 'test@test.com',
+			password: 'password',
+		});
+
+		expect(response.status).toBe(HttpStatusCode.Created);
+	});
+
+	it('should return 400 with an invalid email', async () => {
+		const response = await request(app).post('/api/users/signup').send({
+			email: 'invalid email',
+			password: 'password',
+		});
+
+		expect(response.status).toBe(HttpStatusCode.BadRequest);
+	});
+
+	it('should return 400 with an invalid password', async () => {
+		const response = await request(app).post('/api/users/signup').send({
+			email: 'test@test.com',
+			password: 'p',
+		});
+
+		expect(response.status).toBe(HttpStatusCode.BadRequest);
+	});
+
+	it('should return 400 with missing email and password', async () => {
+		let response = await request(app).post('/api/users/signup').send({});
+
+		expect(response.status).toBe(HttpStatusCode.BadRequest);
+
+		response = await request(app).post('/api/users/signup').send({ email: 'test@test.com' });
+
+		expect(response.status).toBe(HttpStatusCode.BadRequest);
+
+		response = await request(app).post('/api/users/signup').send({ password: 'password' });
+
+		expect(response.status).toBe(HttpStatusCode.BadRequest);
+	});
+});
