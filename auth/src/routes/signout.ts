9a890265a5b927219c0428d08ebbba09811dca53
Christofer Assis <chris.f.assis18@gmail.com>
2023-10-05 21:58:54 -0300

    feat: add signOut route implementation

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,5 +1,9 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 
 export const signOutRouter = Router();
 
-signOutRouter.post('/signOut', (req, res) => {});
+signOutRouter.post('/signOut', (request: Request, response: Response) => {
+	request.session = null;
+
+	response.send({});
+});
